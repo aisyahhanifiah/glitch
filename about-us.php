c1fbde10b396a6860fd5146c4966603354cc4bdc
aisyahhanifiah <aisyahhanifiah@live.com>
2018-10-17 16:52:25 +1000

    Add files via upload

---
+++ about-us.php
@@ -0,0 +1,58 @@
+<?php 
+
+$page_title = 'Culture Fever';
+include_once('header.php'); 
+
+?>
+<!-- About Section -->
+<section class="success about" id="about">
+	<div class="container">
+		<div class="row">
+			<div class="col-lg-6 col-lg-offset-3 text-center" id="introtext">
+				<p>Say Goodbye To Culture Shock</p>
+			</div>
+		</div>
+
+		<div class="row">
+			<div class="col-lg-8 col-lg-offset-2">
+
+				<div id="code">
+					<div id="groupcircle">
+						<div id="red" class="circle"></div>
+						<div id="yellow" class="circle"></div>
+						<div id="green" class="circle"></div>
+					</div>
+
+
+					<div id="console">
+						<p><center>About Culture Fever</center><br><br>
+							<strong>Bonjour handsome 👦 and beautiful! 👧</strong><br><br>
+
+                                        Loves to travel abroad?✈️ But……. having some cultural difficulties while you are in your trips?👉👈 Or shy to ask the locals? 😰
+                                        Or you don’t understand the information from internet 📱 <br><br>
+
+                                        Say no more ❌ to cultural difficulties!<br><br>
+
+                                        We created Culture Fever just to help you when you are travelling 💼 and having some cultural difficulties.
+                                        Culture Fever provide guides for you to be more understand ✔️ the culture in your visiting country 🗺️.
+                                        All you have to do just choose ☝️ your visiting country 📍 and choose what difficulties you wish to know ❓❓ and wallahhh! 🎉
+                                        And the information you need is here! ℹ️<br><br>
+
+                                        Hmm.. what if you are not visiting any country? 😔 Eager to know others’ country culture? 🤗 Or you are doing some research on others’ country culture? 🗺️<br><br>
+
+                                        Never mind.💁 You also can use Culture Fever! Feel free to explore more on others’ country culture on through Culture Fever! 💖<br><br>
+
+                                        Love 💕,<br>
+                                        Culture Fever Team 💐
+                                  </div>
+                            </div>
+
+                      </div>
+                </div>
+          </div>
+    </section>
+<?php 
+
+include_once('footer.php'); 
+
+?>
